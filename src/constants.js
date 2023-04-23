098db3c5f80eb5c7ff53c11f02555c6f3ece4d99
Andrey Popkov <popkov.info@gmail.com>
2023-04-23 22:41:13 +0300

    Timeline Data Generation

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,7 @@ import { PhClock, PhListBullets, PhChartBar } from '@phosphor-icons/vue'
 export const PAGE_TIMELINE = 'выполняемые'
 export const PAGE_ACTIVITIES = 'активные'
 export const PAGE_PROGRESS = 'прогресс'
+export const HOURS_IN_DAY = 24
 
 export const NAV_ITEMS = {
   [PAGE_TIMELINE]: PhClock,
