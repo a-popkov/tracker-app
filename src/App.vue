833f29353c030f6be6d523028e0249aad376948b
Andrey Popkov <popkov.info@gmail.com>
2023-04-25 23:52:53 +0300

    Add Selected

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <TheHeader @go-to-timeline="goTo(PAGE_TIMELINE)" @go-to-progress="goTo(PAGE_PROGRESS)" />
   <main class="flex flex-col flex-grow ">
-    <TheTimeline v-show="currentPage === PAGE_TIMELINE" />
+    <TheTimeline v-show="currentPage === PAGE_TIMELINE" :timeline-items="timelineItems" />
     <TheActivities v-show="currentPage === PAGE_ACTIVITIES" />
     <TheProgress v-show="currentPage === PAGE_PROGRESS" />
   </main>
@@ -17,9 +17,10 @@ import TheTimeline from './page/TheTimeline.vue'
 import TheActivities from './page/TheActivities.vue'
 import TheProgress from './page/TheProgress.vue'
 import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from './constants'
-import { normalizePageHash } from './function'
+import { normalizePageHash, generateTimelineItems } from './function'
 
 const currentPage = ref(normalizePageHash())
+const timelineItems = generateTimelineItems()
 
 function goTo (page) {
   currentPage.value = page
--- src/components/TimelineItem.vue
+++ src/components/TimelineItem.vue
@@ -1,11 +1,43 @@
 <template>
   <li class="relative flex flex-col gap-2 border-t border-gray-200 py-10 px-4">
-    <a href="#" :class="hourLinkClasses">{{ timelineItem.hour }}:00 </a>
+    <a href="#" :class="hourLinkClasses">{{ timelineItem.hour }}:00</a>
+    <div class="flex gap-2">
+      <button
+        class="rounded p-3 bg-gray-100 enabled:hover:bg-gray-200 disabled:cursor-not-allowed disabled:opacity-50"
+      >
+        <Ph-X class="h-8" />
+      </button>
+      <select name="" id="" class="w-full truncate rounded bg-gray-100 py-1 px-2 text-xl">
+        <option selected disabled value="">Отдых</option>
+        <option v-for="{ value, label } in options" :key="value" :value="value">
+          {{ label }}</option
+        >
+        <!-- <option value="">Программирование</option>
+        <option value="">Спорт</option>
+        <option value="">Обучение</option> -->
+      </select>
+    </div>
   </li>
 </template>
 
 <script setup>
+import { PhX } from '@phosphor-icons/vue'
+
 const props = defineProps(['timelineItem'])
+const options = [
+  {
+    value: 1,
+    label: 'Программирование'
+  },
+  {
+    value: 2,
+    label: 'Спорт'
+  },
+  {
+    value: 3,
+    label: 'Обучение'
+  }
+]
 
 const hourLinkClasses = [
   'absolute -top-4 left-1/2 -translate-x-1/2 rounded px-2 font-mono text-lg',
