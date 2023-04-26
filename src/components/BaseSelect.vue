9b6f9f48a4349dee7b141dedde988054d1a80664
Andrey Popkov <popkov.info@gmail.com>
2023-04-26 20:32:30 +0300

    Add Button component

---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex gap-2">
+    <BaseButton>
+      <Ph-X class="h-8" />
+    </BaseButton>
+    <select class="w-full truncate rounded bg-gray-100 py-1 px-2 text-xl">
+      <option selected disabled value="">{{ placeholder }}</option>
+      <option
+        v-for="{ value, label } in options"
+        :key="value"
+        :value="value"
+        :selected="value === selected"
+      >
+        {{ label }}</option
+      >
+    </select>
+  </div>
+</template>
+
+<script setup>
+import BaseButton from './BaseButton.vue'
+import { PhX } from '@phosphor-icons/vue'
+
+defineProps(['selected', 'options', 'placeholder'])
+</script>
--- src/components/TimelineItem.vue
+++ src/components/TimelineItem.vue
@@ -1,27 +1,12 @@
 <template>
   <li class="relative flex flex-col gap-2 border-t border-gray-200 py-10 px-4">
     <a href="#" :class="hourLinkClasses">{{ timelineItem.hour }}:00</a>
-    <div class="flex gap-2">
-      <button
-        class="rounded p-3 bg-gray-100 enabled:hover:bg-gray-200 disabled:cursor-not-allowed disabled:opacity-50"
-      >
-        <Ph-X class="h-8" />
-      </button>
-      <select name="" id="" class="w-full truncate rounded bg-gray-100 py-1 px-2 text-xl">
-        <option selected disabled value="">Отдых</option>
-        <option v-for="{ value, label } in options" :key="value" :value="value">
-          {{ label }}</option
-        >
-        <!-- <option value="">Программирование</option>
-        <option value="">Спорт</option>
-        <option value="">Обучение</option> -->
-      </select>
-    </div>
+    <BaseSelect :selected="selectedActivityId" :options="options" placeholder="Программирование" />
   </li>
 </template>
 
 <script setup>
-import { PhX } from '@phosphor-icons/vue'
+import BaseSelect from './BaseSelect.vue'
 
 const props = defineProps(['timelineItem'])
 const options = [
@@ -45,4 +30,6 @@ const hourLinkClasses = [
     ? 'bg-purple-900 font-black text-white'
     : 'bg-gray-100 text-gray-500'
 ]
+
+const selectedActivityId = 2
 </script>
